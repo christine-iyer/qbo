9b537072c3217aafd9f048e76b733e83b99009f7
chrisIyer <hellohotbox420@gmail.com>
2024-07-30 17:09:32 -0400

    router is working.

--- src/components/QuickBooksAuth.js
+++ src/components/QuickBooksAuth.js
@@ -1,6 +1,15 @@
+import React, { useEffect } from 'react';
+import axios from 'axios';
+import { useHistory } from 'react-router-dom';
+
+const CLIENT_ID = 'your-client-id';
+const CLIENT_SECRET = 'your-client-secret';
+const REDIRECT_URI = 'your-redirect-uri';
 
 
-const QuickBooksAuth = () => (
+const QuickBooksAuth = () => 
+  
+  (
   <h1>Lets get you authenticateed</h1>
 )
 
