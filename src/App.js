27361530c7565c81fe1ce8483d25f78a6334ecb1
chrisIyer <hellohotbox420@gmail.com>
2025-08-06 15:24:22 -0400

    decorated.

--- src/App.js
+++ src/App.js
@@ -6,10 +6,14 @@ import ManageInvoices from './components/ManageInvoices';
 import ItemsList from './components/ItemsList';
 import DeliveryReport from './components/DeliveryReport';
 import Nav from './components/Nav';
+import okeyDokey from './okeydonkey.PNG';
 function App(){
   return (
     <div className="App">
       <Nav/>
+      <img src={okeyDokey} alt="Okey Dokey Logo" style={{ width: '200px', margin: '20px' }} />
+      <h1>Okey Donkey Integration</h1>
+      <p>Welcome to the Okey Donkey. Use the navigation above to access different features.</p>
 
     <Routes>
       <Route path="/" element={<QuickBooksAuth />} />
