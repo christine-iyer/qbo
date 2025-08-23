6edaad2f28fd9064fb75d2ee7a0ad9e2cff89d1b
chrisIyer <hellohotbox420@gmail.com>
2025-08-23 19:01:22 -0400

    updated delivery destinations.

--- src/components/data.jsx
+++ src/components/data.jsx
@@ -1,277 +1,393 @@
 const customersToImport = [
      {
 
-     name: "Midcoast Vegan Big Apple",
-     ShipAddr: {
-          Line1: "111 Pleasant St",
-          City: "Brunswick",
-          CountrySubDivisionCode: "ME",
-          PostalCode: "04011"
+          name: "Midcoast Vegan Big Apple",
+          ShipAddr: {
+               Line1: "111 Pleasant St",
+               City: "Brunswick",
+               CountrySubDivisionCode: "ME",
+               PostalCode: "04011"
+          },
      },
-},
-{
-     name: "Morning Glory",
-     ShipAddr: {
-          Line1: "60 Main St",
-          City: "Brunswick",
-          CountrySubDivisionCode: "ME",
-          PostalCode: "04011"
-     }},
      {
-     name: "Wildflowers Gluten Free Bakery",
-     ShipAddr: {
-          Line1: "54 Cumberland St,",
-          City: "Brunswick",
-          CountrySubDivisionCode: "ME",
-          PostalCode: "04011"
-     }},
+          name: "Morning Glory",
+          ShipAddr: {
+               Line1: "60 Main St",
+               City: "Brunswick",
+               CountrySubDivisionCode: "ME",
+               PostalCode: "04011"
+          }
+     },
      {
-     name: "Sundrenched (Front & Linden)",
-     ShipAddr: {
-          Line1: "244 Front St,",
-          City: "Bath",
-          CountrySubDivisionCode: "ME",
-          PostalCode: "04530"
-     }},
-      {
-     name: "Kennebec Meat Co.",
-     ShipAddr: {
-          Line1: "215 Water St,",
-          City: "Bath",
-          CountrySubDivisionCode: "ME",
-          PostalCode: "04530"
-     }},
-   
-   {
-     name: "Bath Natural Market",
-     ShipAddr: {
-          Line1: "36 Centre St",
-          City: "Bath",
-          CountrySubDivisionCode: "ME",
-          PostalCode: "04530"
-     }},
-
-{
-     name: "Rising Tide",
-     ShipAddr: {
-          Line1: "323 Main St",
-          City: "Damariscotta",
-          CountrySubDivisionCode: "ME",
-          PostalCode: "04543"
+          name: "Wildflowers Gluten Free Bakery",
+          ShipAddr: {
+               Line1: "54 Cumberland St",
+               City: "Brunswick",
+               CountrySubDivisionCode: "ME",
+               PostalCode: "04011"
+          }
      },
-},
-{
-     name: "Pemaquid Falls Farm",
-     ShipAddr: {
-          Line1: "123 Pemaquid Falls Rd",
-          City: "Bristol",
-          CountrySubDivisionCode: "ME",
-          PostalCode: "04539"
-     }
-},
-{
-     name: "Sammys's Deluxe",
-     ShipAddr: {
-          Line1: "488 Main St",
-          City: "Rockland",
-          CountrySubDivisionCode: "ME",
-          PostalCode: "04841"
-     }
-},   
-{
-     name: "Good Tern",
-     ShipAddr: {
-          Line1: "750 Main Street",
-          City: "Rockland",
-          CountrySubDivisionCode: "ME",
-          PostalCode: "04841"
+     {
+          name: "Sundrenched (Front & Linden)",
+          ShipAddr: {
+               Line1: "244 Front St",
+               City: "Bath",
+               CountrySubDivisionCode: "ME",
+               PostalCode: "04530"
+          }
      },
-},
-{
-     name: "Tenderwild",
-     ShipAddr: {
-          Line1: "33 Gurney St",
-          City: "Rockport",
-          CountrySubDivisionCode: "ME",
-          PostalCode: "04856"
+     {
+          name: "Kennebec Meat Co.",
+          ShipAddr: {
+               Line1: "215 Water St",
+               City: "Bath",
+               CountrySubDivisionCode: "ME",
+               PostalCode: "04530"
+          }
      },
-},
-{
-     name: "Fresh off the Farm",
-     ShipAddr: {
-          Line1: "495 Commercial St",
-          City: "Rockport",
-          CountrySubDivisionCode: "ME",
-          PostalCode: "04856"
-     },
-},
-{
-     name: "3 Bug Farm",
-     ShipAddr: {
-          Line1: "11 Wiley Neck Rd",
-          City: "Lincolnville",
-          CountrySubDivisionCode: "ME",
-          PostalCode: "04849"
-     }},
 
      {
-     name: "Owen's General Store",
-     ShipAddr: {
-          Line1: "269 Main Street",
-          City: "Lincolnville",
-          CountrySubDivisionCode: "ME",
-          PostalCode: "04849"
+          name: "Bath Natural Market",
+          ShipAddr: {
+               Line1: "36 Centre St",
+               City: "Bath",
+               CountrySubDivisionCode: "ME",
+               PostalCode: "04530"
+          }
      },
-},
 
-{name: "Ararat Farms",
-     ShipAddr: {
-          Line1: "55 Vancycle Road",
-          City: "Lincolnville",
-          CountrySubDivisionCode: "ME",
-          PostalCode: "04849"
-     }
-},
- {
-     name: "Marsh River",
-     ShipAddr: {
-          Line1: "5 Veterans Highway",
-          City: "Brooks",
-          CountrySubDivisionCode: "ME",
-          PostalCode: "04921"
-     }},
-     {name: "Graze",
-     ShipAddr: {
-          Line1: "28 Spring St",
-          City: "Belfast",
-          CountrySubDivisionCode: "ME",
-          PostalCode: "04915"
-     }},
-   {
-     name: "Belfast Co-op",
-     ShipAddr: {
-          Line1: "123 high St",
-          City: "Belfast",
-          CountrySubDivisionCode: "ME",
-          PostalCode: "04915"
-     }},
-     {name: "Stockton Harbor (Stockton Springs Town Office)",
-     ShipAddr: {
-          Line1: "217 Main St",
-          City: "Stockton Springs",
-          CountrySubDivisionCode: "ME",
-          PostalCode: "04981"
-     }},
-     {name: "Farm Drop (Blue Hill Berry Co)",
-     ShipAddr: {
-          Line1: "123 Main St",
-          City: "Blue Hill",
-          CountrySubDivisionCode: "ME",
-          PostalCode: "04614"
-     }},
-     {name: "Dennett's on the Wharf (Penobscot Volunteer Fire Department)",
-     ShipAddr: {
-          Line1: "123 Main St",
-          City: "Penobscot",
-          CountrySubDivisionCode: "ME",
-          PostalCode: "04476"
-     }},
-   {
-     name: "Blue Hill Coop",
-     ShipAddr: {
-          Line1: "70 South St",
-          City: "Blue Hill",
-          CountrySubDivisionCode: "ME",
-          PostalCode: "04614"
-     }},
-     
-      {
-     name: "Blue Hill Hannaford",
-     ShipAddr: {
-          Line1: "16 South St",
-          City: "Blue Hill",
-          CountrySubDivisionCode: "ME",
-          PostalCode: "04614"
-     }},
-   {
-     name: "Megunticook",
-     ShipAddr: {
-          Line1: "2 Gould St",
-          City: "Camden",
-          CountrySubDivisionCode: "ME",
-          PostalCode: "04843"
-     },
-},
-
-
-   {
-     name: "Bahner Farm",
-     ShipAddr: {
-          Line1: "153 Augusta Rd",
-          City: "Belmont",
-          CountrySubDivisionCode: "ME",
-          PostalCode: "04952"
-     }},
-  
      {
-     name: "French and Braun",
-     ShipAddr: {
-          Line1: "1 Elm Street",
-          City: "Camden",
-          CountrySubDivisionCode: "ME",
-          PostalCode: "04843"
+          name: "Rising Tide",
+          ShipAddr: {
+               Line1: "323 Main St",
+               City: "Damariscotta",
+               CountrySubDivisionCode: "ME",
+               PostalCode: "04543"
+          },
      },
-},
-   {
-     name: "Blue Hill Wine Shop",
-     ShipAddr: {
-          Line1: "138 Main St",
-          City: "Blue Hill",
-          CountrySubDivisionCode: "ME",
-          PostalCode: "04614"
-     }},
      {
-     name: "Buck's (Blue Hill Wine Shop)",
-     ShipAddr: {
-          Line1: "138 Main St",
-          City: "Blue Hill",
-          CountrySubDivisionCode: "ME",
-          PostalCode: "04614"
-     }},
-   {
-     name: "Masons",
-     ShipAddr: {
-          Line1: "15 Hardy St",
-          City: "Brewer",
-          CountrySubDivisionCode: "ME",
-          PostalCode: "04412"
-     }},
-   {
-     name: "Tiller and Rye",
-     ShipAddr: {
-          Line1: " 20 S Main St",
-          City: "Brewer",
-          CountrySubDivisionCode: "ME",
-          PostalCode: "04412"
-     }},
-   {
-     name: "Natural Living Center",
-     ShipAddr: {
-          Line1: "209 Longview Dr,",
-          City: "Bangor",
-          CountrySubDivisionCode: "ME",
-          PostalCode: "04401"
-     }},
-   {
-     name: "Uncle Dean's",
-     ShipAddr: {
-          Line1: "80 Grove St",
-          City: "Waterville",
-          CountrySubDivisionCode: "ME",
-          PostalCode: "04901"
-     }
-   
-}]
+          name: "Pemaquid Falls Farm",
+          ShipAddr: {
+               Line1: "123 Pemaquid Falls Rd",
+               City: "Bristol",
+               CountrySubDivisionCode: "ME",
+               PostalCode: "04539"
+          }
+     },
+     {
+          name: "Sammys's Deluxe",
+          ShipAddr: {
+               Line1: "488 Main St",
+               City: "Rockland",
+               CountrySubDivisionCode: "ME",
+               PostalCode: "04841"
+          }
+     },
+     {
+          name: "Good Tern",
+          ShipAddr: {
+               Line1: "750 Main Street",
+               City: "Rockland",
+               CountrySubDivisionCode: "ME",
+               PostalCode: "04841"
+          },
+     },
+     {
+          name: "Tenderwild",
+          ShipAddr: {
+               Line1: "33 Gurney St",
+               City: "Rockport",
+               CountrySubDivisionCode: "ME",
+               PostalCode: "04856"
+          },
+     },
+     {
+          name: "Fresh off the Farm",
+          ShipAddr: {
+               Line1: "495 Commercial St",
+               City: "Rockport",
+               CountrySubDivisionCode: "ME",
+               PostalCode: "04856"
+          },
+     },
+     {
+          name: "3 Bug Farm",
+          ShipAddr: {
+               Line1: "11 Wiley Neck Rd",
+               City: "Lincolnville",
+               CountrySubDivisionCode: "ME",
+               PostalCode: "04849"
+          }
+     },
+
+     {
+          name: "Owen's General Store",
+          ShipAddr: {
+               Line1: "269 Main Street",
+               City: "Lincolnville",
+               CountrySubDivisionCode: "ME",
+               PostalCode: "04849"
+          },
+     },
+
+     {
+          name: "Ararat Farms",
+          ShipAddr: {
+               Line1: "55 Vancycle Road",
+               City: "Lincolnville",
+               CountrySubDivisionCode: "ME",
+               PostalCode: "04849"
+          }
+     },
+     {
+          name: "Marsh River",
+          ShipAddr: {
+               Line1: "5 Veterans Highway",
+               City: "Brooks",
+               CountrySubDivisionCode: "ME",
+               PostalCode: "04921"
+          }
+     },
+     {
+          name: "Graze",
+          ShipAddr: {
+               Line1: "28 Spring St",
+               City: "Belfast",
+               CountrySubDivisionCode: "ME",
+               PostalCode: "04915"
+          }
+     },
+     {
+          name: "Belfast Co-op",
+          ShipAddr: {
+               Line1: "123 high St",
+               City: "Belfast",
+               CountrySubDivisionCode: "ME",
+               PostalCode: "04915"
+          }
+     },
+     {
+          name: "Stockton Harbor (Stockton Springs Town Office)",
+          ShipAddr: {
+               Line1: "217 Main St",
+               City: "Stockton Springs",
+               CountrySubDivisionCode: "ME",
+               PostalCode: "04981"
+          }
+     },
+     {
+          name: "Farm Drop (Blue Hill Berry Co)",
+          ShipAddr: {
+               Line1: "123 Main St",
+               City: "Blue Hill",
+               CountrySubDivisionCode: "ME",
+               PostalCode: "04614"
+          }
+     },
+     {
+          name: "Dennett's on the Wharf (Penobscot Volunteer Fire Department)",
+          ShipAddr: {
+               Line1: "123 Main St",
+               City: "Penobscot",
+               CountrySubDivisionCode: "ME",
+               PostalCode: "04476"
+          }
+     },
+     {
+          name: "Blue Hill Coop",
+          ShipAddr: {
+               Line1: "70 South St",
+               City: "Blue Hill",
+               CountrySubDivisionCode: "ME",
+               PostalCode: "04614"
+          }
+     },
+
+     {
+          name: "Blue Hill Hannaford",
+          ShipAddr: {
+               Line1: "16 South St",
+               City: "Blue Hill",
+               CountrySubDivisionCode: "ME",
+               PostalCode: "04614"
+          }
+     },
+
+     {
+          name: "Blue Hill Wine Shop",
+          ShipAddr: {
+               Line1: "138 Main St",
+               City: "Blue Hill",
+               CountrySubDivisionCode: "ME",
+               PostalCode: "04614"
+          }
+     },
+     {
+          name: "Buck's (Blue Hill Wine Shop)",
+          ShipAddr: {
+               Line1: "138 Main St",
+               City: "Blue Hill",
+               CountrySubDivisionCode: "ME",
+               PostalCode: "04614"
+          }
+     },
+     {
+          name: "Rooster Brother",
+          ShipAddr: {
+               Line1: " 29 Main St",
+               City: "Ellsworth",
+               CountrySubDivisionCode: "ME",
+               PostalCode: "04605"
+          }
+     },
+     {
+          name: "Flexit",
+          ShipAddr: {
+               Line1: " 142 Main St",
+               City: "Ellsworth",
+               CountrySubDivisionCode: "ME",
+               PostalCode: "04605"
+          }
+     },
+     {
+          name: "John Edward's",
+          ShipAddr: {
+               Line1: " 158 Main St",
+               City: "Ellsworth",
+               CountrySubDivisionCode: "ME",
+               PostalCode: "04605"
+          }
+     },
+     {
+          name: "Black Moon Cafe",
+          ShipAddr: {
+               Line1: " 142 Main St",
+               City: "Ellsworth",
+               CountrySubDivisionCode: "ME",
+               PostalCode: "04605"
+          }
+     },
+     {
+          name: "Triple Chick Farm (behind John Edward's)",
+          ShipAddr: {
+               Line1: " 158 Main St",
+               City: "Ellsworth",
+               CountrySubDivisionCode: "ME",
+               PostalCode: "04605"
+          }
+     },
+     {
+          name: "Darthia Farm (behind John Edward's)",
+          ShipAddr: {
+               Line1: " 158 Main St",
+               City: "Ellsworth",
+               CountrySubDivisionCode: "ME",
+               PostalCode: "04605"
+          }
+     },
+     {
+          name: "Masons",
+          ShipAddr: {
+               Line1: "15 Hardy St",
+               City: "Brewer",
+               CountrySubDivisionCode: "ME",
+               PostalCode: "04412"
+          }
+     },
+     {
+          name: "Tiller and Rye",
+          ShipAddr: {
+               Line1: " 20 S Main St",
+               City: "Brewer",
+               CountrySubDivisionCode: "ME",
+               PostalCode: "04412"
+          }
+     },
+     {
+          name: "Miguel's",
+          ShipAddr: {
+               Line1: " 20 S Main St",
+               City: "Brewer",
+               CountrySubDivisionCode: "ME",
+               PostalCode: "04412"
+          }
+     },
+     {
+          name: "Natural Living Center",
+          ShipAddr: {
+               Line1: "209 Longview Dr",
+               City: "Bangor",
+               CountrySubDivisionCode: "ME",
+               PostalCode: "04401"
+          }
+     },
+     {
+          name: "Kanu",
+          ShipAddr: {
+               Line1: " 293 Main St",
+               City: "Old Town",
+               CountrySubDivisionCode: "ME",
+               PostalCode: "04468"
+          }
+     },
+     {
+          name: "Tipped Trailer",
+          ShipAddr: {
+               Line1: " 25 Water St",
+               City: "Newport",
+               CountrySubDivisionCode: "ME",
+               PostalCode: "04953"
+          }
+     },
+     // {
+     //      name: "Daybreak Growers Alliance",
+     //      ShipAddr: {
+     //           Line1: "",
+     //           City: "Unity",
+     //           CountrySubDivisionCode: "ME",
+     //           PostalCode: "04988"
+     //      }
+     // },
+     {
+          name: "Uncle Dean's",
+          ShipAddr: {
+               Line1: "80 Grove St",
+               City: "Waterville",
+               CountrySubDivisionCode: "ME",
+               PostalCode: "04901"
+          }
+
+     },
+     // {
+     //      name: "Megunticook",
+     //      ShipAddr: {
+     //           Line1: "2 Gould St",
+     //           City: "Camden",
+     //           CountrySubDivisionCode: "ME",
+     //           PostalCode: "04843"
+     //      },
+     // },
+
+
+     // {
+     //      name: "Bahner Farm",
+     //      ShipAddr: {
+     //           Line1: "153 Augusta Rd",
+     //           City: "Belmont",
+     //           CountrySubDivisionCode: "ME",
+     //           PostalCode: "04952"
+     //      }
+     // },
+
+     // {
+     //      name: "French and Braun",
+     //      ShipAddr: {
+     //           Line1: "1 Elm Street",
+     //           City: "Camden",
+     //           CountrySubDivisionCode: "ME",
+     //           PostalCode: "04843"
+     //      }},
+     ]
 export default customersToImport;
 //      Midcoast Vegan (pick up at Big Apple in Brunswick)
 // Morning Glory Natural Foods
