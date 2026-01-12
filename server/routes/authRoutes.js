bb08c4b942a119af5a0ef5d7417c54ddf4a0bb04
chrisIyer <hellohotbox420@gmail.com>
2026-01-12 13:37:25 -0500

    removed unnes=cessary files/

--- server/routes/authRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const authController = require('../controllers/authController');
-
-router.get('/auth/callback', authController.handleCallback);
-router.get('/auth/status', authController.checkAuth);
-
-module.exports = router;
